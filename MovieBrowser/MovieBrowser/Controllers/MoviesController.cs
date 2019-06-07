228d744a9866c4e5bac506cb414588c01a119b11
Birat Lamichhane <birat.lamichhane@selu.edu>
2019-06-06 22:15:13 -0500

    Added Customer Controller defining its viewModel for its UI

--- MovieBrowser/MovieBrowser/Controllers/MoviesController.cs
+++ MovieBrowser/MovieBrowser/Controllers/MoviesController.cs
@@ -5,37 +5,36 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MovieBrowser.Models;
 using System.Text.RegularExpressions;
-
+using MovieBrowser.ViewModel;
 namespace MovieBrowser.Controllers
 {
     public class MoviesController : Controller
     {
-        public ViewResult Random()
+        public ActionResult Random()
         {
             var movie = new Movie()
             {
                 Name = "Shrek!"
             };
+            var customers = new List<Customer>
+            {
+                new Customer { Name="Customer 1"},
+                new Customer { Name="Customer 2"}
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
 
-            return View(movie);
+            return View(viewModel);
         }
-        //public ActionResult Edit(int id) 
-        //{
-        //    return Content("id=" + id);
-        //}
-        //public ActionResult Index(int? pageIndex, string sortBy)
-        //{
-        //    if (!pageIndex.HasValue)
-        //        pageIndex = 1;
-        //    if (String.IsNullOrWhiteSpace(sortBy))
-        //        sortBy = "Name";
-        //    return Content(String.Format("pageIndex={0}&sortBy={1}",pageIndex,sortBy));
-        //}
+       
 
-        [Route(@"Movies/ByReleasedDate/{year}/{month:regex(\\d{4}):range(1,12)}")]
-        public ActionResult ByReleasedDate (int year, int month)
-        {
-            return Content(year + "/" + month);
-        }
+        //[Route(@"Movies/ByReleasedDate/{year}/{month:regex(\\d{4}):range(1,12)}")]
+        //public ActionResult ByReleasedDate (int year, int month)
+        //{
+        //    return Content(year + "/" + month);
+        //}
     }
 }
--- MovieBrowser/MovieBrowser/Models/Customer.cs
+++ MovieBrowser/MovieBrowser/Models/Customer.cs
@@ -5,11 +5,9 @@ using System.Threading.Tasks;
 
 namespace MovieBrowser.Models
 {
-    public class Movie
+    public class Customer
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        
     }
-
 }
